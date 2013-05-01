0d6acca819f581fcf3d15c751d572a4f07112e23
Rudy Pena <rudypenajr@gmail.com>
2013-04-30 22:29:33 -0500

    verifying gem was updated

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -3,7 +3,8 @@ require "rulers/version"
 module Rulers
   class Application
     def call(env)
-      [200, {'Content-Type'} => 'text/html', ['Hello from Ruby on Rulers!']]
+      [200, {'Content-Type'} => 'text/html', 
+        ['Hello from Ruby on Rulers!']]
     end
   end
 end
