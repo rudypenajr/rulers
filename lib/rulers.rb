5b2b15cb0681bbe95d14e625e404bd8aecbd10d1
Rudy Pena <rudypenajr@gmail.com>
2013-05-01 14:53:03 -0500

    adding debugging to ruler.rb

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -3,8 +3,9 @@ require "rulers/version"
 module Rulers
   class Application
     def call(env)
-      [200, {'Content-Type' => 'text/html'},
-        ['Hello from Ruby on Rulers!']]
+      'echo debug > debut.txt';
+        [200, {'Content-Type' => 'text/html'},
+          ['Hello from Ruby on Rulers!']]
     end
   end
 end
